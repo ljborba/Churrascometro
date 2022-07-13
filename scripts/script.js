f72d076edf4a19620d90429f8b93bde60f575b95
Lucas Janing <janinglucas123@gmail.com>
2022-07-13 03:04:41 -0300

    3.0 javascript structure

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("Calcular");
+}
